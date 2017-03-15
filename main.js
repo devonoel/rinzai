e1ea62c9654f9276a1006aa42d8e88c98111f38c
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2017-03-14 23:02:03 -0400

    Add vue.js, get basic html/js frontend set up

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var app = new Vue({
   el: '#todos',
   data: {
-    todos: ['Do thing', 'Do other thing']
+    todos: ['Do thing', 'Do other thing'],
+    newTodo: '',
+    result: ''
+  },
+
+  methods: {
+    add: function() {
+      var e = this.newTodo;
+      if (!e) return;
+      this.todos.push(e);
+    },
+
+    getRandom: function() {
+      this.result = "Result: " + this.todos[Math.floor(Math.random() * this.todos.length)];
+    }
   }
 });
